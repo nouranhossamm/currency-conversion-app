a7890f7fceda2365888338e8d6f28be49bc101c0
Muhammad Bassiouni <bassiouni.git@gmail.com>
2023-08-22 02:09:03 +0300

    initial commit

---
+++ src/test/java/com/banquemisr/currencyconversionapp/CurrencyConversionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquemisr.currencyconversionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
