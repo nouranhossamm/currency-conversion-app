9e7e7af12b95017ed6e148b7052cf404826730f9
Nouran Hosameldin <111601276+nouranhossamm@users.noreply.github.com>
2023-08-26 18:48:58 +0300

    Update CurrencyConversionAppApplication.java

--- src/main/java/com/banquemisr/currencyconversionapp/CurrencyConversionAppApplication.java
+++ src/main/java/com/banquemisr/currencyconversionapp/CurrencyConversionAppApplication.java
@@ -3,9 +3,11 @@ package com.banquemisr.currencyconversionapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCaching
 public class CurrencyConversionAppApplication {
 
     public static void main(String[] args) {
